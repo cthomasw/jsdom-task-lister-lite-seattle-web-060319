69e1021a2464b1fbdcd5856bf8cda9a64a7f97c4
Claire Wilbert <claire.wilbert@gmail.com>
2019-07-17 09:09:33 -0700

    first solo attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.querySelector('#create-task-form')
+  document.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const newTaskDescription = document.querySelector('#new-task-description');
+    makeTask(newTaskDescription)
+  })
+  
+  const userNameLabel = document.createElement('label');
+  userNameLabel.innerText = "User:"
+  const userNameField = document.createElement('input');
+  userNameField.innerText = 'text';
+  taskForm.insertBefore(userNameLabel, taskForm.childNodes[0]);
+  taskForm.insertBefore(userNameField, taskForm.childNodes[1]);
 });
+
+function makeTask(newTaskDescription) {
+  const newTaskItem = document.createElement('li');
+  const listTasks = document.querySelector('#list');
+  listTasks.appendChild(newTaskItem);
+  newTaskItem.innerText = newTaskDescription.value;
+  newTaskDescription.value = '';
+}
+
+
+
+
