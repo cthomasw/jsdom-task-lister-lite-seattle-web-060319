7bd13df20280ef4d2c1b07be678fc72ca9de20a8
Claire Wilbert <claire.wilbert@gmail.com>
2019-07-19 08:10:24 -0700

    second solo attempt

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
+const newItemForm = document.querySelector('#create-task-form');
+let dueDateLabel = document.createElement('label');
+dueDateLabel.innerText = "Due date:";
+let dueDateField = document.createElement('input');
+dueDateField.setAttribute('id', 'new-task-due');
+dueDateField.setAttribute('placeholder', 'due date');
+dueDateField.setAttribute('type', 'text');
+newItemForm.insertBefore(dueDateField, newItemForm.childNodes[0]);
+newItemForm.insertBefore(dueDateLabel, newItemForm.childNodes[0]);
 
+
+
+newItemForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  let dateDue = (e.target[0].value);
+  let item = (e.target[1].value);
+  makeNewItem(item, dateDue);
+  e.target[0].value = '';
+  e.target[1].value = '';
+});
+
+function makeNewItem(item, dateDue) {
+  let ul = document.querySelector('#tasks');
+  let li = document.createElement('li');
+
+  li.innerText = `${item} Due: ${dateDue}`;
+  ul.appendChild(li);
+  const itemComplete = document.createElement('button');
+  itemComplete.innerText = 'Done!';
+  li.appendChild(itemComplete);
+  itemComplete.addEventListener('click', function(e){
+    ul.removeChild(li);
+  })
+};
